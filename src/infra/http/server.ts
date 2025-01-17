9819be204e44a0ef4f83f5bb5e16c04c5ef79ca7
Vitor Mancini <vitor.mancini21@hotmail.com>
2025-01-17 16:55:49 -0300

    config: enviroment variables setup

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,12 @@
+import { env } from "@/env";
 import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
 
 const server = fastify();
 
 server.register(fastifyCors, { origin: "*" });
+
+console.log(env.DATABASE_URL);
 
 server
   .listen({
