816aad66d466a12476bbc1b22c2a737516e9f48a
Vitor Mancini <vitor.mancini21@hotmail.com>
2025-01-21 18:02:46 -0300

    feat: export-uploads end export-uploads test

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -8,6 +8,7 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
+import { exportUploadsRoute } from "./routes/export-uploads";
 import { getUploadsRoute } from "./routes/get-uploads";
 import { uploadImageRoute } from "./routes/upload-images";
 import { transformSwaggerSchema } from "./transform-swagger-schema";
@@ -50,6 +51,7 @@ server.register(fastifySwaggerUi, {
 //Rotas
 server.register(uploadImageRoute);
 server.register(getUploadsRoute);
+server.register(exportUploadsRoute);
 
 server
   .listen({
